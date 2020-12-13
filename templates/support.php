4e077700f58254a5738262b05198e9d05e6e21c1
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2020-12-13 15:41:31 +0530

    Add files via upload

---
+++ templates/support.php
@@ -0,0 +1,44 @@
+<section id="info" class="info-section">
+<div class="container text-xs-center">
+         <!-- Section Header -->
+                <div class="col-md-12 col-sm-12 col-xs-12 section-header wow fadeInDown">
+                    <h2><span class="highlight-text">Support Section</span></h2>
+                   
+                    <p class="col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">You can support us in many ways !</p>
+                </div>
+                <!-- Section Header End -->
+        <div class="row wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+            <div class="col-md-3 col-sm-6 col-xs-12 text-xs-center">
+                
+                <i class="fa fa-thumbs-o-up" style="font-size:36px"></i>
+                
+                <h1 class="text-primary">Promote Us</h1>
+                <h4>You can promote us in the ways you can !</h4>
+            </div>
+
+
+            <div class="col-md-3 col-sm-6 col-xs-12 text-xs-center">
+                
+                <i class="fa fa-users" style="font-size:36px"></i>
+                
+                <h1 class="text-primary">Support Us</h1>
+                <h4>Support Us in the way you can!</h4>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 text-xs-center">
+              
+                <i class="fa fa-heart" style="font-size:36px;"></i>
+                
+               <h1 class="text-primary">Volunteer</h1>
+               <h4>Help us find Volunteers for empowering others</h4> 
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 text-xs-center">
+                
+                <i class="fa fa-thumbs-o-up" style="font-size:36px"></i>
+                
+                <h1 class="text-primary">Sponsor us!</h1>
+                <h4>You can find sponsors for  us in the ways you can !</h4>
+            </div>
+            
+        </div> 
+    </div>
+</section>
---
+++ templates/team.php
@@ -0,0 +1,54 @@
+<section id="team" class="bgSection teams-section"> 
+<div class="parallax-overlay"></div>
+        <div class="teams-wrapper wow bounceIn">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Section Header -->
+                    <div class="col-md-12 col-sm-12 col-xs-12 section-header wow fadeInDown">
+                        <h2><span class="highlight-text-inverted">Core Team </span></h2>
+                       
+                        <p class="col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">The greatest minds of this college formed this club and this club is the strength of the college.</p>
+                    </div>
+
+                    <!-- teams Slider -->
+                    <div id="teams" class="owl-carousel teams">
+
+                        <!-- Slides -->
+                        <div class="teams-slides col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">
+                            <img src="images/img-teams/team2.jpg" alt="">
+							<p class="client-info">Vishwajeet Singh Yadav</p>
+                            <p>Founder</p>
+                            <p>Student working towards a Bachelor of Technology (B.Tech) focused in Information Technology from IIIT Bhopal.</p>
+                            <p><i class="fa fa-github" style="font-size:36px"></i>
+                            <i class="fa fa-linkedin-square" style="font-size:36px"></i></p>
+                        </div>
+                        <!-- Slides End -->
+
+                        <!-- Slides -->
+                        <div class="teams-slides col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">
+                            <img src="images/img-teams/team2.jpg" alt="">
+							<p class="client-info">Shubham Singh</p>
+                            <p>Co-Founder</p>
+                            <p>Student working towards a Bachelor of Technology (B.Tech) focused in Information Technology from IIIT Bhopal.</p>
+                            <p><i class="fa fa-github" style="font-size:36px"></i>
+                            <i class="fa fa-linkedin-square" style="font-size:36px"></i></p>
+                        </div>
+                        <!-- Slides End -->
+
+                        <!-- Slides -->
+                        <div class="teams-slides col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">
+                            <img src="images/img-teams/team2.jpg" alt="">
+							 <p class="client-info">Anmol Porwal</p>
+                            <p>Associate Founder</p>
+                            <p>Student working towards a Bachelor of Technology (B.Tech) focused in Information Technology from IIIT Bhopal.</p>
+                            <p><i class="fa fa-github" style="font-size:36px"></i>
+                            <i class="fa fa-linkedin-square" style="font-size:36px"></i></p>
+                        </div>
+                        <!-- Slides End -->
+
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </section> 
